3e950424074c8737201925912fd0e1f8f251aa52
Sean Ross-Ross <srossross@gmail.com>
2024-08-07 13:58:04 -0400

    execute

--- run_script.sh
+++ run_script.sh
@@ -1,8 +1,20 @@
 #!/usr/bin/env bash
 
-echo "ENVIRONMENT"
-env
+# The number of tasks defined in the --tasks parameter.
+export TOTAL_PARTITIONS="${CLOUD_RUN_TASK_COUNT}"
 
-echo "ARGUMENTS"
-echo $@
+# The index of this task. Starts at 0 for the first task and increments by 1 
+# for every successive task, up to the maximum number of tasks minus 1. 
+# If you set --parallelism to greater than 1, tasks might not follow the index order. 
+# For example, it would be possible for task 2 to start before task 1.
+export CURRENT_PARTITION="${CLOUD_RUN_TASK_INDEX}"
 
+
+echo "--- ENVIRONMENT ---"
+echo "OUTPUT_ROOT=${OUTPUT_ROOT}"
+echo "TOTAL_PARTITIONS=${TOTAL_PARTITIONS}"
+echo "CURRENT_PARTITION=${CURRENT_PARTITION}"
+echo "ARGUMENTS" $@
+
+# TODO: 
+# Call python -m "score.cli" $@
