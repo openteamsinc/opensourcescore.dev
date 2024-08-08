c2707f7f86ae5a23ce55ec0c214f5116d7bcc44b
Siddhesh Ghadi <sghadi03@gmail.com>
2024-08-08 11:53:33 +0530

    Update cli.py

--- score/cli.py
+++ score/cli.py
@@ -1,13 +1,16 @@
-import click
-import string
 import os
+import string
 from pathlib import Path
-from .logger import setup_logger
-from .data_retrieval.json_scraper import scrape_json
-from .data_retrieval.web_scraper import scrape_web
-from .utils.get_pypi_package_list import get_pypi_package_names
+
+import click
+
 from .conda.get_conda_package_names import get_conda_package_names
 from .conda.scrape_conda import scrape_conda
+from .data_retrieval.json_scraper import scrape_json
+from .data_retrieval.web_scraper import scrape_web
+from .logger import setup_logger
+from .utils.get_pypi_package_list import get_pypi_package_names
+from .vulnerabilities.scrape_vulnerabilities import scrape_vulnerabilities
 
 # from .data_retrieval.github_scraper import scrape_github_data
 
@@ -150,6 +153,47 @@ def conda(num_partitions, partition, output, channel):
 
     click.echo(f"Saving data to {output}")
     df.to_parquet(output, partition_cols=["channel", "partition"])
+    click.echo("Scraping completed.")
+
+
+@cli.command()
+@click.option(
+    "--output",
+    default=OUTPUT_ROOT / "output" / "vulnerabilities",
+    help="The output directory to save the scraped data in hive partition",
+)
+@click.option(
+    "-e",
+    "--ecosystem",
+    default="PyPI",
+    help="The ecosystem to scrape vulnerabilities for",
+)
+@click.option(
+    "-n",
+    "--num-partitions",
+    required=True,
+    type=int,
+    help="The number of partitions in total.",
+)
+@click.option(
+    "-p",
+    "--partition",
+    required=True,
+    type=int,
+    help="The partition number to process.",
+)
+def vulnerabilities(num_partitions, partition, output, ecosystem):
+    packages = get_pypi_package_names(num_partitions, partition)
+    click.echo(
+        f"Will process {len(packages)} packages in partition {partition} of {num_partitions}"
+    )
+
+    df = scrape_vulnerabilities(packages, ecosystem)
+    df["partition"] = partition
+    df["ecosystem"] = ecosystem
+
+    click.echo(f"Saving data to {output}")
+    df.to_parquet(output, partition_cols=["ecosystem", "partition"])
     click.echo("Scraping completed.")
 
 
