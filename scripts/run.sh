76a0213be5ee6582b29fe1208826fda680137e5d
Sean Ross-Ross <srossross@gmail.com>
2024-08-13 10:17:43 -0700

    update tasks

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,30 +3,33 @@
 
 
 
-export DATE="$(date +%Y-%m-%d)"
+export DATE="$(date +%Y-%m-%d).1"
 
 export ENVVARS="OUTPUT_ROOT=gcs://openteams-score-data/${DATE}"
 echo "using DATE=$DATE"
 
+date
+gcloud --project=openteams-score \
+    run jobs execute --region=us-west1 \
+    scrape-conda \
+    --update-env-vars "${ENVVARS}"  \
+    --wait
 
-# gcloud --project=openteams-score \
-#     run jobs execute --region=us-west1 \
-#     scrape-conda \
-#     --update-env-vars "${ENVVARS}"  \
-#     --wait
+date
+gcloud --project=openteams-score \
+    run jobs execute --region=us-west1 \
+    scrape-pypi \
+    --update-env-vars "${ENVVARS}"  \
+    --wait
 
-# gcloud --project=openteams-score \
-#     run jobs execute --region=us-west1 \
-#     scrape-pypi \
-#     --update-env-vars "${ENVVARS}"  \
-#     --wait
+date
+gcloud --project=openteams-score \
+    run jobs execute --region=us-west1 \
+    agg-source-urls \
+    --update-env-vars "${ENVVARS}"  \
+    --wait
 
-# gcloud --project=openteams-score \
-#     run jobs execute --region=us-west1 \
-#     agg-source-urls \
-#     --update-env-vars "${ENVVARS}"  \
-#     --wait
-
+date
 gcloud --project=openteams-score \
     run jobs execute --region=us-west1 \
     scrape-git \
