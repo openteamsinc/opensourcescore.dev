755e828190e1a3184a8eff23b44e0d58affc1af6
Sean Ross-Ross <srossross@gmail.com>
2024-08-13 08:17:35 -0700

    update pypi

---
+++ scripts/run.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -xeo pipefail
+
+
+
+
+export DATE="$(date +%Y-%m-%d)"
+
+export ENVVARS="OUTPUT_ROOT=gcs://openteams-score-data/${DATE}"
+echo "using DATE=$DATE"
+
+
+gcloud --project=openteams-score \
+    run jobs execute --region=us-west1 \
+    scrape-conda \
+    --update-env-vars "${ENVVARS}"  \
+    --wait
+
+gcloud --project=openteams-score \
+    run jobs execute --region=us-west1 \
+    scrape-pypi \
+    --update-env-vars "${ENVVARS}"  \
+    --wait
+
+gcloud --project=openteams-score \
+    run jobs execute --region=us-west1 \
+    agg-source-urls \
+    --update-env-vars "${ENVVARS}"  \
+    --wait
