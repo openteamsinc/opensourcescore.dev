e3b1c36d140bccd264b021228e4af1f326c5269b
Sean Ross-Ross <srossross@gmail.com>
2024-08-13 12:42:07 -0700

    fix time col (#68)
    * fix time col
    
    * fix git urls

--- score/git_vcs/scrape.py
+++ score/git_vcs/scrape.py
@@ -15,6 +15,7 @@ def scrape_git(urls: list) -> pd.DataFrame:
     all_data = []
     for url in tqdm(urls, disable=None):
         info = get_info_from_git_repo(url)
+        info["source_url"] = url
         all_data.append(info)
 
     return pd.DataFrame(all_data)
@@ -62,9 +63,8 @@ def get_info_from_git_repo(url) -> dict:
         max_monthly_authors_count = rolling_authors.max()
 
         return {
-            "source_url": url,
             "recent_authors_count": recent_authors_count,
             "max_monthly_authors_count": max_monthly_authors_count,
-            "first_commit": commits.when.min().isoformat(),
-            "latest_commit": commits.when.max().isoformat(),
+            "first_commit": commits.when.min(),
+            "latest_commit": commits.when.max(),
         }
