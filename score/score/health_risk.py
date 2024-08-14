1f1357baa11dee56999ec9716d7185c3154091fa
Sean Ross-Ross <srossross@gmail.com>
2024-08-14 06:32:59 -0700

    updated score

--- score/score/health_risk.py
+++ score/score/health_risk.py
@@ -1,4 +1,14 @@
-def build_health_risk_score(source_url, row):
+def build_health_risk_score(source_url, git_info):
     score = {"value": "Healthy", "notes": []}
+
+    if git_info.error:
+        score["value"] = "Unkonwn"
+        score["notes"].append(git_info.error)
+        return score
+
+    if git_info.first_commit == "NaT":
+        score["value"] = "Placeholder"
+        score["notes"].append("There are no human commits in this repository")
+        return score
 
     return score
