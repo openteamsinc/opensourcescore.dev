d660f33fe8109d71046fc3026e654d6dc26b131c
Siddhesh Ghadi <sghadi03@gmail.com>
2024-08-19 13:17:22 +0530

    updated code with api instead of web scraping

--- score/npm/scrape_npm.py
+++ score/npm/scrape_npm.py
@@ -1,13 +1,30 @@
-import json
+from datetime import datetime
 from typing import List
 
 import pandas as pd
-from bs4 import BeautifulSoup
 from tqdm import tqdm
 
 from ..utils.request_session import get_session
 
-NPM_PACKAGE_TEMPLATE_URL = "https://www.npmjs.com/package/{package_name}"
+current_date = datetime.now().strftime("%Y-%m-%d")
+
+NPM_PACKAGE_TEMPLATE_URL = "https://registry.npmjs.org/{package_name}"
+NPM_PACKAGE_DOWNLOAD_URL = (
+    "https://api.npmjs.org/downloads/range/2000-01-01:{current_date}/{package_name}"
+)
+
+
+def get_npm_package_downloads(package_name: str) -> int:
+    s = get_session()
+    downloads_url = NPM_PACKAGE_DOWNLOAD_URL.format(
+        current_date=current_date, package_name=package_name
+    )
+    downloads_res = s.get(downloads_url)
+    downloads_res.raise_for_status()
+    downloads_data = downloads_res.json()
+    total_downloads = sum(day["downloads"] for day in downloads_data["downloads"])
+    ndownloads = total_downloads if total_downloads else 0
+    return ndownloads
 
 
 def scrape_npm(package_names: List[str]) -> pd.DataFrame:
@@ -17,40 +34,24 @@ def scrape_npm(package_names: List[str]) -> pd.DataFrame:
         url = NPM_PACKAGE_TEMPLATE_URL.format(package_name=package)
         res = s.get(url)
         res.raise_for_status()
-        soup = BeautifulSoup(res.content, "html.parser")
-        script_tag_data = soup.select_one("script")
+        package_data = res.json()
 
-        if script_tag_data.has_attr("integrity"):
-            json_data = (
-                str(script_tag_data.text).lstrip("window.__context__ = ").rstrip(";")
-            )
-            data = json.loads(json_data)
+        ndownloads = get_npm_package_downloads(package)
+        maintainers_count = (
+            len(package_data.get("maintainers", [])) if package_data else 0
+        )
 
-            context = data.get("context", {})
-            package_data = context.get("packument", {})
-
-            ndownloads = (
-                sum([i.get("downloads") for i in context.get("downloads", {})])
-                if context.get("downloads", {})
-                else 0
-            )
-            maintainers_count = (
-                len(package_data.get("maintainers", [])) if package_data else 0
-            )
-
-            all_packages.append(
-                {
-                    "name": package,
-                    "full_name": context.get("package"),
-                    "source_url": (
-                        package_data.get("repository")
-                        if package_data.get("repository")
-                        else None
-                    ),
-                    "latest_version": package_data.get("version"),
-                    "ndownloads": ndownloads,
-                    "maintainers_count": maintainers_count,
-                }
-            )
+        all_packages.append(
+            {
+                "name": package,
+                "full_name": package_data.get("package"),
+                "source_url": package_data.get("repository", None)
+                .get("url")
+                .lstrip("git+"),
+                "latest_version": package_data.get("dist-tags", {}).get("latest"),
+                "ndownloads": ndownloads,
+                "maintainers_count": maintainers_count,
+            }
+        )
 
     return pd.DataFrame(all_packages)
