505f1be8ca6af9478f49ee49ffe8ade29a468fd4
Siddhesh Ghadi <sghadi03@gmail.com>
2024-08-20 17:48:28 +0530

    created process for fetching data from api

--- score/npm/get_npm_package_names.py
+++ score/npm/get_npm_package_names.py
@@ -1,20 +1,40 @@
 import json
+import logging
+import multiprocessing
+import os
 from hashlib import sha256
 from typing import List, Tuple
 
 from ..utils.request_session import get_session
 
+log = logging.getLogger(__name__)
+
 NPM_PACKAGE_URL = "https://replicate.npmjs.com/_all_docs"
 
+NPM_PACKAGE_NAMES_FILE = "npm_package_names.json"
 
-def fetch_npm_package_names(
-    limit: int = 1000, start_key: str = None, end_key: str = None
-) -> Tuple[List[str], str]:
+
+def save_npm_package_names_to_file(all_packages: List[str]) -> None:
+    with open(NPM_PACKAGE_NAMES_FILE, "w") as f:
+        json.dump(all_packages, f)
+
+
+def load_npm_package_names_from_file() -> List[str]:
+    if not os.path.exists(NPM_PACKAGE_NAMES_FILE):
+        with open(NPM_PACKAGE_NAMES_FILE, "w") as f:
+            f.write("[]")
+        print(f"Created file: {NPM_PACKAGE_NAMES_FILE}")
+        return []
+
+    with open(NPM_PACKAGE_NAMES_FILE, "r") as f:
+        all_packages = json.load(f)
+        return all_packages
+
+
+def fetch_npm_package_names(limit: int, start_key: str = None) -> Tuple[List[str], str]:
     params = {"limit": limit, "include_docs": False}
     if start_key:
         params["startkey"] = json.dumps(start_key)
-    if end_key:
-        params["endkey"] = json.dumps(end_key)
     s = get_session()
     res = s.get(NPM_PACKAGE_URL, params=params)
     res.raise_for_status()
@@ -26,21 +46,33 @@ def fetch_npm_package_names(
 
 def get_all_npm_package_names() -> List[str]:
     all_package_names = []
-    start_key = None
-    end_key = None
+    existing_packages = load_npm_package_names_from_file()
+    start_key = existing_packages[-1] if existing_packages else None
     while True:
         package_names, start_key = fetch_npm_package_names(
-            limit=1000, start_key=start_key, end_key=end_key
+            limit=100000, start_key=start_key
         )
         if not package_names:
             break
-        all_package_names.extend(package_names)
-        print(f"\rFound {len(set(all_package_names))} package names", end="")
-    return list(set(all_package_names))
+        all_package_names = list(set(existing_packages + package_names))
+        save_npm_package_names_to_file(all_package_names)
+        print(f"Found {len(all_package_names)} package names")
+    return all_package_names
+
+
+def run_process():
+    # Create processes for fetching all npm package names
+    fetch_process = multiprocessing.Process(target=get_all_npm_package_names)
+    fetch_process.start()
+
+    # load all packages from file whichever is available
+    all_packages = load_npm_package_names_from_file()
+
+    return all_packages
 
 
 def get_npm_package_names(num_partitions: int, partition: int) -> List[str]:
-    all_packages = get_all_npm_package_names()
+    all_packages = run_process()
 
     def is_in_partition(name: str):
         package_hash = sha256(name.encode()).hexdigest()
