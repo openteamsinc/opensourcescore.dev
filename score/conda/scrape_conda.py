b1684a35fdd0f7fd78368b9eb175d243e77f4398
Igor Derke <igoorderke@gmail.com>
2024-08-21 15:48:10 -0500

    added schemas for each data source (#80)
    * added schemas for each data source
    * updated docstrings to better describe data returned by each scraper

--- score/conda/scrape_conda.py
+++ score/conda/scrape_conda.py
@@ -8,6 +8,23 @@ CONDA_PACKAGE_URL_TEMPLATE = "https://api.anaconda.org/package/{channel}/{packag
 
 
 def scrape_conda(channel, package_names: List[str]) -> pd.DataFrame:
+    """
+    Scrapes metadata for a list of Conda packages from the Anaconda API and returns the data as a DataFrame.
+
+    Args:
+        channel (str): The Anaconda channel from which to retrieve the package data.
+        package_names (List[str]): A list of package names to scrape data for.
+
+    Returns:
+        pd.DataFrame: A DataFrame containing metadata for each package. The DataFrame includes the following fields:
+
+        - `name` (str): The name of the package.
+        - `full_name` (str): The full name of the package, typically including the channel and package name.
+        - `source_url` (Optional[str]): The URL to the source code repository, if available.
+                This is taken from either `dev_url` or `source_git_url`.
+        - `latest_version` (str): The latest version of the package available on the specified Anaconda channel.
+        - `ndownloads` (int): The total number of downloads for all versions of the package.
+    """
     s = get_session()
     all_packages = []
     for package in tqdm(package_names, disable=None):
