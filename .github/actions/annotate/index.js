a23560291a1bf125557e885c7a4aac08d43dc0cb
karamba228 <igoorderke@gmail.com>
2024-09-13 12:39:56 -0500

    testing action for publication

--- .github/actions/annotate/index.js
+++ .github/actions/annotate/index.js
@@ -1,16 +1,43 @@
 const fs = require('fs');
-const axios = require('axios');
+const https = require('https');
 
 // Function to strip versioning from package names
 function stripVersion(packageLine) {
     return packageLine.split(/[<>=~!]/)[0].trim();
 }
 
+// Function to make an API request using the built-in https module
+function makeApiRequest(packageName) {
+    const url = `https://openteams-score.vercel.app/api/package/pypi/${packageName}`;
+
+    return new Promise((resolve, reject) => {
+        https.get(url, (res) => {
+            let data = '';
+
+            // Collect the data as it comes in
+            res.on('data', (chunk) => {
+                data += chunk;
+            });
+
+            // End of the response
+            res.on('end', () => {
+                if (res.statusCode === 200) {
+                    resolve(JSON.parse(data));
+                } else {
+                    reject(`Request failed with status code ${res.statusCode}`);
+                }
+            });
+        }).on('error', (e) => {
+            reject(`Error: ${e.message}`);
+        });
+    });
+}
+
 async function annotatePackage(packageName) {
     try {
-        const response = await axios.get(`https://openteams-score.vercel.app/api/package/pypi/${packageName}`);
-        if (response.status === 200 && response.data.source) {
-            const { maturity, health_risk } = response.data.source;
+        const response = await makeApiRequest(packageName);
+        if (response && response.source) {
+            const { maturity, health_risk } = response.source;
             const maturityValue = maturity ? maturity.value : 'Unknown';
             const healthRiskValue = health_risk ? health_risk.value : 'Unknown';
             
@@ -19,7 +46,7 @@ async function annotatePackage(packageName) {
             console.log(`::error file=requirements.txt::Package ${packageName} not found.`);
         }
     } catch (error) {
-        console.log(`::error file=requirements.txt::Error looking up package ${packageName}: ${error.message}`);
+        console.log(`::error file=requirements.txt::Error looking up package ${packageName}: ${error}`);
     }
 }
 
