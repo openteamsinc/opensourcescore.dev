7eb2449a9c72125021902434fc849d598f0b3a40
Sean Ross-Ross <srossross@gmail.com>
2024-12-03 14:04:35 -0800

    update score

--- score/app.py
+++ score/app.py
@@ -1,27 +1,31 @@
-import numpy as np
-from typing import Any
+import os
 from fastapi import FastAPI
-from .pypi.json_scraper import get_package_data
-from .conda.scrape_conda import get_conda_package_data
-from .git_vcs.scrape import create_git_metadata
+
+from .app_utils import (
+    get_conda_package_data_cached,
+    get_pypi_package_data_cached,
+    create_git_metadata_cached,
+    convert_numpy_types,
+)
+
 
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"version": os.environ.get("K_REVISION", "?")}
 
 
 @app.get("/pypi/{package_name}")
-async def pypi(package_name):
-    data = get_package_data(package_name)
+def pypi(package_name):
+    data = get_pypi_package_data_cached(package_name)
     return {"ecosystem": "pypi", "package_name": package_name, "data": data}
 
 
 @app.get("/conda/{channel}/{package_name}")
-async def conda(channel, package_name):
-    data = get_conda_package_data(channel, package_name)
+def conda(channel, package_name):
+    data = get_conda_package_data_cached(channel, package_name)
     return {
         "ecosystem": "conda",
         "channel": channel,
@@ -30,30 +34,8 @@ async def conda(channel, package_name):
     }
 
 
-def convert_numpy_types(obj: Any) -> Any:
-    # Handle numpy types
-    if isinstance(obj, np.bool_):
-        return bool(obj)
-    if isinstance(obj, np.integer):
-        return int(obj)
-    if isinstance(obj, np.floating):
-        return float(obj)
-    if isinstance(obj, np.ndarray):
-        return obj.tolist()
-
-    # Handle nested dictionaries
-    if isinstance(obj, dict):
-        return {key: convert_numpy_types(value) for key, value in obj.items()}
-
-    # Handle lists, tuples, and other iterables
-    if isinstance(obj, (list, tuple, set)):
-        return type(obj)(convert_numpy_types(item) for item in obj)
-
-    return obj
-
-
 @app.get("/git/{source_url:path}")
-async def git(source_url):
-    data = create_git_metadata(source_url)
+def git(source_url):
+    data = create_git_metadata_cached(source_url)
     data = convert_numpy_types(data)
     return {"source_url": source_url, "data": data}
---
+++ score/app_utils.py
@@ -0,0 +1,41 @@
+from typing import Any
+
+import numpy as np
+from cachetools import cached, TTLCache
+from .pypi.json_scraper import get_package_data as get_pypi_package_data
+from .conda.scrape_conda import get_conda_package_data
+from .git_vcs.scrape import create_git_metadata
+
+
+get_pypi_package_data_cached = cached(cache=TTLCache(maxsize=2**18, ttl=60 * 60))(
+    get_pypi_package_data
+)
+
+get_conda_package_data_cached = cached(cache=TTLCache(maxsize=2**18, ttl=60 * 60))(
+    get_conda_package_data
+)
+create_git_metadata_cached = cached(cache=TTLCache(maxsize=2**18, ttl=60 * 60))(
+    create_git_metadata
+)
+
+
+def convert_numpy_types(obj: Any) -> Any:
+    # Handle numpy types
+    if isinstance(obj, np.bool_):
+        return bool(obj)
+    if isinstance(obj, np.integer):
+        return int(obj)
+    if isinstance(obj, np.floating):
+        return float(obj)
+    if isinstance(obj, np.ndarray):
+        return obj.tolist()
+
+    # Handle nested dictionaries
+    if isinstance(obj, dict):
+        return {key: convert_numpy_types(value) for key, value in obj.items()}
+
+    # Handle lists, tuples, and other iterables
+    if isinstance(obj, (list, tuple, set)):
+        return type(obj)(convert_numpy_types(item) for item in obj)
+
+    return obj
