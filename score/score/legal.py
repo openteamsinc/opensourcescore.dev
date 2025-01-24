ec24ad8ed37a18a9617aeb0fab88a08b14f7a207
Sean Ross-Ross <srossross@gmail.com>
2025-01-24 12:48:30 -0800

    Updated categories to be in notes

--- score/score/legal.py
+++ score/score/legal.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-from .score_type import Score, MODERATE_RISK, CAUTION_NEEDED
+from .score_type import Score
 
 from ..notes import Note
 
@@ -13,30 +13,30 @@ def score_license(git_info: dict, score: Score):
     modified = license.get("modified")
 
     if license.get("error"):
-        score.limit(MODERATE_RISK)
-        note = license.get("error", Note.NO_LICENSE_INFO.value)
-        score.notes.append(note)
+        # score.limit(MODERATE_RISK)
+        note = license.get("error", Note.NO_LICENSE_INFO)
+        # score.notes.append(note)
+        score.add_note(note)
 
     elif not license_kind or license_kind == "Unknown":
-        score.limit(MODERATE_RISK)
-        note = license.get("error", Note.NO_OS_LICENSE.value)
-        score.notes.append(note)
+        score.add_note(Note.NO_OS_LICENSE)
 
     if license_kind in LESS_PERMISSIVE_LICENSES:
-        score.limit(CAUTION_NEEDED)
-        score.notes.append(Note.LESS_PERMISSIVE_LICENSE.value)
+        score.add_note(Note.LESS_PERMISSIVE_LICENSE)
+        # score.limit(CAUTION_NEEDED)
+        # score.notes.append(Note.LESS_PERMISSIVE_LICENSE.value)
 
     if modified:
-        score.limit(CAUTION_NEEDED)
-        score.notes.append(Note.LICENSE_MODIFIED.value)
+        score.add_note(Note.LICENSE_MODIFIED)
+        # score.limit(CAUTION_NEEDED)
+        # score.notes.append(Note.LICENSE_MODIFIED.value)
 
 
 def build_legal_score(git_info: dict) -> Score:
     score = Score()
 
     if git_info.get("error") and not pd.isna(git_info["error"]):
-        score.value = "Unknown"
-        score.notes.append(git_info["error"])
+        score.add_note(git_info["error"])
         return score
 
     score_license(git_info, score)
