2df9458ce8c8e7c5f836f9358a0f67d34594eadb
Sean Ross-Ross <srossross@gmail.com>
2025-04-10 06:53:46 -0700

    logging

--- score/app_utils.py
+++ score/app_utils.py
@@ -78,20 +78,28 @@ def get_package_data_cached(
         cached_pkg = load_from_cache(Package, cache_filename)
         if cached_pkg is not None:
             append_header("pkg-cache-hit", "true")
+            log.info(
+                f"Cache hit for {ecosystem}/{package_name}",
+                extra={
+                    "package_lookup": "yes",
+                    "ecosystem": ecosystem,
+                    "package_name": package_name,
+                },
+            )
+
             return cached_pkg
 
-    log.info(f"Cache miss for {ecosystem}/{package_name}")
-    append_header("pkg-cache-hit", "false")
-    pkg = get_package_data(ecosystem, package_name)
-
-    save_to_cache(pkg, cache_filename)
-
     log.info(
-        f"Getting package data for {ecosystem}/{package_name}",
+        f"Cache miss for {ecosystem}/{package_name}",
         extra={
+            "package_lookup": "yes",
             "ecosystem": ecosystem,
             "package_name": package_name,
         },
     )
+    append_header("pkg-cache-hit", "false")
+    pkg = get_package_data(ecosystem, package_name)
+
+    save_to_cache(pkg, cache_filename)
 
     return pkg
