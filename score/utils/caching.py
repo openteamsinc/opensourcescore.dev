24951bb7f2307d7eb80d2041ab46e6aa70a2a511
Sean Ross-Ross <srossross@gmail.com>
2025-04-25 13:34:58 -0700

    bust the cache and add better logs

--- score/utils/caching.py
+++ score/utils/caching.py
@@ -17,6 +17,22 @@ fs = fsspec.filesystem(protocol)
 log = logging.getLogger(__name__)
 
 
+def pretty_time_delta(seconds: int):
+    sign_string = "-" if seconds < 0 else ""
+    seconds = abs(int(seconds))
+    days, seconds = divmod(seconds, 86400)
+    hours, seconds = divmod(seconds, 3600)
+    minutes, seconds = divmod(seconds, 60)
+    if days > 0:
+        return "%s%dd%dh%dm%ds" % (sign_string, days, hours, minutes, seconds)
+    elif hours > 0:
+        return "%s%dh%dm%ds" % (sign_string, hours, minutes, seconds)
+    elif minutes > 0:
+        return "%s%dm%ds" % (sign_string, minutes, seconds)
+    else:
+        return "%s%ds" % (sign_string, seconds)
+
+
 def cache_path(suffix: str):
     return f"{CACHE_LOCATION}/{suffix}"
 
@@ -37,6 +53,7 @@ def cache_hit(filename, days=1):
         raise ValueError(f"mtime is not a datetime: {type(mtime)}{mtime}")
 
     age = datetime.now(tz=timezone.utc) - mtime
+    log.info(f"Cache age: {pretty_time_delta(age.total_seconds())}")
     return age.days <= days
 
 
